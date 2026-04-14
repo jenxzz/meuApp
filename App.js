83d2a1000b6be32d460e75a2cbd96884aa75834c
jenxzz <mariaeduardamatoslopes57@gmail.com>
2026-04-14 09:50:34 -0300

    api

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [id, setId] = useState('');
+  const [nome, setNome] = useState('Nome aparecerá aqui');
+  const [profissao, setProfissao] = useState('Profissão aparecerá aqui');
+  const [carregando, setCarregando] = useState(false);
+
+  async function buscarUserAsync() {
+    try {
+      setCarregando(true);
+      const response = await fetch(`http://localhost:3000/usuario/${id}`);
+      const dados = await response.json();
+
+      setNome(dados.nome);
+      setProfissao(dados.profissao);
+      setCarregando(false);
+    } catch (erro) {
+      console.log("Erro:", erro);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {carregando ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+          <Text style={styles.text}>{nome}</Text>
+          <Text style={styles.text}>{profissao}</Text>
+
+          <TextInput
+            placeholder="Digite o ID (1 ou 2)"
+            value={id}
+            onChangeText={setId}
+            style={styles.input}
+          />
+
+          <TouchableOpacity style={styles.button} onPress={buscarUserAsync}>
+            <Text style={styles.buttonText}>Buscar usuário</Text>
+          </TouchableOpacity>
+        </>
+      )}
     </View>
   );
 }
@@ -13,8 +49,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    marginBottom: 10,
+    fontSize: 16
+  },
+  input: {
+    borderWidth: 1,
+    width: '60%',
+    marginBottom: 10,
+    padding: 5
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5
+  },
+  buttonText: {
+    color: 'white'
+  }
 });
